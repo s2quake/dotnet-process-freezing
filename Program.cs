4fbaf1a1d5c12000096bb1d057bf7e52520bc983
s2quake <han0210@netsgo.com>
2024-11-03 21:37:39 +0900

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var json = GetString(args[0]);
+        Console.WriteLine(json);
+    }
+
+    private static string GetString(string fileName)
+    {
+        var process = new Process();
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.FileName = $"jq";
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.Arguments = $". -C \"{fileName}\"";
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.Start();
+        process.WaitForExit();
+        return process.StandardOutput.ReadToEnd();
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+### Requirement
+
+brew install jq
+
+### Freezing
+
+dotnet build && dotnet run jq1.json
+
+### Success
+
+dotnet build && dotnet run jq2.json
